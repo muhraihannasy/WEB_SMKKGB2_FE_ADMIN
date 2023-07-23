3af427823e7eb18532689f88d3b7aca5395515ea
muhraihannasy <muhraihannasy@gmail.com>
2023-07-23 20:35:41 +0700

    add comment to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react';
 
+// Type
 type ButtonSizeType = {
   sm: string;
   md: string;
@@ -12,6 +13,15 @@ type ButtonBackgroundType = {
   tertiary: string;
 };
 
+// Interface
+interface ButtonProps {
+  size: keyof ButtonSizeType;
+  bg: keyof ButtonBackgroundType;
+  children: ReactNode;
+  active?: boolean;
+  onClick?: () => void;
+}
+
 const ButtonSize: ButtonSizeType = {
   sm: 'px-10 py-4',
   md: 'px-8 px-10',
@@ -21,16 +31,8 @@ const ButtonSize: ButtonSizeType = {
 const ButtonBackground: ButtonBackgroundType = {
   primary: 'bg-primary',
   secondary: 'bg-meta-3',
-  tertiary: 'bg-',
+  tertiary: 'bg-black',
 };
-
-interface ButtonProps {
-  size: keyof ButtonSizeType;
-  bg: keyof ButtonBackgroundType;
-  children: ReactNode;
-  active?: boolean;
-  onClick?: () => void;
-}
 
 const Button: React.FC<ButtonProps> = ({ children, bg, size, onClick }) => {
   const styles = `inline-flex items-center justify-center gap-2.5 rounded-md  text-center font-medium text-white hover:bg-opacity-90  
